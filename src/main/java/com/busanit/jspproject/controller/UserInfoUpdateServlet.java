10768e3b48f86722a472a52dcd6f7bebc14af02e
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-09-25 14:48:32 +0900

    - 회원가입시 도로명 주소 API 사용 - 마이페이지, 회원정보 수정 기능

--- src/main/java/com/busanit/jspproject/controller/UserInfoUpdateServlet.java
+++ src/main/java/com/busanit/jspproject/controller/UserInfoUpdateServlet.java
@@ -1,4 +1,7 @@
 package com.busanit.jspproject.controller;
+import com.busanit.jspproject.dao.UserDAO;
+import com.busanit.jspproject.dto.UserVO;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -8,12 +11,31 @@ import java.io.IOException;
 public class UserInfoUpdateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
+        String url = "/mypage-update.jsp";
+        request.getRequestDispatcher(url).forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        UserVO user = new UserVO();
+        user.setUserID(request.getParameter("user_id"));
+        user.setPassword(request.getParameter("user_pw"));
+        user.setNickname(request.getParameter("user_nickname"));
+        user.setGender(request.getParameter("gender_select"));
+        user.setAge(Integer.parseInt(request.getParameter("age_select")));
+        user.setLocation(request.getParameter("location_select"));
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.updateUserInfo(user);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+
+        response.sendRedirect("/mypage");
+
 
     }
 }
